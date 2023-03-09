a0ef7a3044dad2873d37a286e3ea64aeec3bb68c
LuizHenriqueT1 <luizhenrique201387@gmail.com>
2023-03-09 16:09:13 -0300

    Revert "Add: sending email with gmail"
    This reverts commit 5c9b41eb04cc00add18d8ca8d3bee5687c6bd487.

--- src/main/java/com/ms/email/repositories/EmailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ms.email.repositories;
-
-import com.ms.email.domain.Email;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmailRepository extends JpaRepository<Email, Long> {
-
-}
