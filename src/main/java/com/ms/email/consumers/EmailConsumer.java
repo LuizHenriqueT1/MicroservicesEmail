bab8cef1e5f4db5b2bfa8d86c57fd171fa3682fe
LuizHenriqueT1 <luizhenrique201387@gmail.com>
2023-03-09 21:52:11 -0300

    Add: sending message with RabbitMQ

---
+++ src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -0,0 +1,25 @@
+package com.ms.email.consumers;
+
+import com.ms.email.domain.Email;
+import com.ms.email.dtos.EmailDto;
+import com.ms.email.services.EmailService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    @Autowired
+    private EmailService emailService;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void listen(@Payload EmailDto emailDto) {
+        Email email = new Email();
+        BeanUtils.copyProperties(emailDto, email);
+        emailService.sendEmail(email);
+        System.out.println("Email Status: " +  email.getStatusEmail().toString());
+    }
+}
--- src/main/java/com/ms/email/repositories/EmailRepository.java
+++ src/main/java/com/ms/email/repositories/EmailRepository.java
@@ -3,6 +3,8 @@ package com.ms.email.repositories;
 import com.ms.email.domain.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmailRepository extends JpaRepository<Email, Long> {
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<Email, UUID> {
 
 }
